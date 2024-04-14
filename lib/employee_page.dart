bcc09ab9a49969180c5d3c4de3080d17d9c532e9
Phil <phil@polarstork.com>
2024-04-14 10:30:54 +0300

    replaced location with geolocator

--- lib/employee_page.dart
+++ lib/employee_page.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 
 class EmployeePage extends StatefulWidget {
   const EmployeePage({super.key, required this.id});
@@ -109,39 +109,46 @@ class _EmployeePageState extends State<EmployeePage> {
     }
   }
 
-  Future<LocationData?> _getLocation() async {
-    Location location = Location();
-
+  Future<Position> _determinePosition() async {
     bool serviceEnabled;
-    PermissionStatus permissionGranted;
+    LocationPermission permission;
 
-    serviceEnabled = await location.serviceEnabled();
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      serviceEnabled = await location.requestService();
-      if (!serviceEnabled) {
-        return null;
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
       }
     }
 
-    permissionGranted = await location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
-        return null;
-      }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
     }
 
-    return location.getLocation();
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 
   Future<void> _updateEmployeeLocation(String employeeId) async {
-    final locationData = await _getLocation();
-
-    if (locationData == null) {
-      return;
-    }
-
     try {
+      final locationData = await _determinePosition();
       final response = await _dio.post(
         '$_baseUrl/location',
         data: {
