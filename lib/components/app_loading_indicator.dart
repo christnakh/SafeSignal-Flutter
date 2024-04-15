a22f27ed005786e569d67a1ac9e3e9518fb03b51
Phil <phil@polarstork.com>
2024-04-16 01:26:40 +0300

    fixes

--- lib/components/app_loading_indicator.dart
+++ lib/components/app_loading_indicator.dart
@@ -5,8 +5,8 @@ class AppLoadingIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
+    return const Scaffold(
+      body: Center(
         child: CircularProgressIndicator(),
       ),
     );
